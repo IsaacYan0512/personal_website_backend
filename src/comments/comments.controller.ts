bcb17283e716879c9c9cd3bbfec8a5da7d6c2ac0
isaacyan3687 <isaacyan3687@gmail.com>
2024-05-06 11:07:16 +1000

    1

--- src/comments/comments.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('connents')
-export class CommentController {
-  @Get()
-  findAll(): string {
-    return 'This action returns all cats';
-  }
-}
